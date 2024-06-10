aeda31f931291adedc72325d2514847c5b871b6b
Qiao Gu <georgegu1997@gmail.com>
2024-06-09 23:21:12 -0400

    add HM3D dataset

--- conceptgraph/dataset/datasets_common.py
+++ conceptgraph/dataset/datasets_common.py
@@ -500,6 +500,15 @@ class ScannetDataset(GradSLAMDataset):
     ):
         self.input_folder = os.path.join(basedir, sequence)
         self.pose_path = None
+
+        # Load the intrinsic matrix from the file in each scene
+        scene_intrinsic_path = os.path.join(self.input_folder, "intrinsic", "intrinsic_color.txt")
+        scene_intrinsic = np.loadtxt(scene_intrinsic_path)
+        config_dict['camera_params']['fx'] = scene_intrinsic[0, 0]
+        config_dict['camera_params']['fy'] = scene_intrinsic[1, 1]
+        config_dict['camera_params']['cx'] = scene_intrinsic[0, 2]
+        config_dict['camera_params']['cy'] = scene_intrinsic[1, 2]
+        
         super().__init__(
             config_dict,
             stride=stride,
@@ -1001,7 +1010,76 @@ class Hm3dDataset(GradSLAMDataset):
             poses.append(pose)
             
         return poses
+    
+class Hm3dOpeneqaDataset(GradSLAMDataset):
+    def __init__(
+        self,
+        config_dict,
+        basedir,
+        sequence,
+        stride: Optional[int] = None,
+        start: Optional[int] = 0,
+        end: Optional[int] = -1,
+        desired_height: Optional[int] = 480,
+        desired_width: Optional[int] = 640,
+        load_embeddings: Optional[bool] = False,
+        embedding_dir: Optional[str] = "embeddings",
+        embedding_dim: Optional[int] = 512,
+        **kwargs,
+    ):
+        self.input_folder = os.path.join(basedir, sequence)
+        self.pose_path = None
 
+        # Load the intrinsic matrix from the file in each scene
+        scene_intrinsic_path = os.path.join(self.input_folder, "intrinsic_color.txt")
+        scene_intrinsic = np.loadtxt(scene_intrinsic_path)
+        config_dict['camera_params']['fx'] = scene_intrinsic[0, 0]
+        config_dict['camera_params']['fy'] = scene_intrinsic[1, 1]
+        config_dict['camera_params']['cx'] = scene_intrinsic[0, 2]
+        config_dict['camera_params']['cy'] = scene_intrinsic[1, 2]
+        
+        super().__init__(
+            config_dict,
+            stride=stride,
+            start=start,
+            end=end,
+            desired_height=desired_height,
+            desired_width=desired_width,
+            load_embeddings=load_embeddings,
+            embedding_dir=embedding_dir,
+            embedding_dim=embedding_dim,
+            **kwargs,
+        )
+        
+    def get_filepaths(self):
+        color_paths = natsorted(glob.glob(f"{self.input_folder}/*-rgb.png"))
+        depth_paths = natsorted(glob.glob(f"{self.input_folder}/*-depth.png"))
+        embedding_paths = None
+        if self.load_embeddings:
+            embedding_paths = natsorted(
+                glob.glob(f"{self.input_folder}/{self.embedding_dir}/*.pt")
+            )
+        return color_paths, depth_paths, embedding_paths
+    
+    def load_poses(self):
+        poses = []
+        posefiles = natsorted(glob.glob(f"{self.input_folder}/[0-9]*.txt"))
+
+        P = torch.tensor(
+            [
+                [1, 0, 0, 0],
+                [0, -1, 0, 0],
+                [0, 0, -1, 0],
+                [0, 0, 0, 1]
+            ]
+        ).float()
+        
+        for posefile in posefiles:
+            pose = torch.from_numpy(np.loadtxt(posefile)).float()
+            pose = P @ pose @ P.T
+            poses.append(pose)
+        return poses
+        
 def load_dataset_config(path, default_path=None):
     """
     Loads config file.
@@ -1107,6 +1185,8 @@ def get_dataset(dataconfig, basedir, sequence, **kwargs):
         return MultiscanDataset(config_dict, basedir, sequence, **kwargs)
     elif config_dict['dataset_name'].lower() in ['hm3d']:
         return Hm3dDataset(config_dict, basedir, sequence, **kwargs)
+    elif config_dict['dataset_name'].lower() in ['hm3d-openeqa']:
+        return Hm3dOpeneqaDataset(config_dict, basedir, sequence, **kwargs)
     else:
         raise ValueError(f"Unknown dataset name {config_dict['dataset_name']}")
 
